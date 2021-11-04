9100cf9e3e830e018add327f4a1e0e7f36582957
wiziwiz <gregory.zu@gmail.com>
2021-11-04 07:54:00 +0000

    add deploy script

---
+++ ethereum/deploy.js
@@ -0,0 +1,29 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+//const { abi, evm } = require("./contracts/build/compainFactory.json");
+
+const compiledFactory = require('./contracts/build/CompainFactory.json');
+
+const mnemonic = '';
+const rinkeby = '';
+
+const provider = new HDWalletProvider(
+    mnemonic,
+    rinkeby
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+  // if not enough gas error, go on inkeby network dashboard and have a 
+  // look at the last block mined : gas used... 
+  const result = await new web3.eth.Contract(compiledFactory.abi)
+    .deploy({ data: compiledFactory.evm.bytecode.object })
+    .send({ gas: '2500000', gasPrice: '5000000000', from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+};
+deploy();
